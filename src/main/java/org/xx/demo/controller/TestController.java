6b91caae14e357bf5f588ea5d32f5cd448a394a6
wuzhijie <1019306750@qq.com>
2020-08-15 22:13:06 +0800

    [feat] 新增读取文件返回功能

--- src/main/java/org/xx/demo/controller/TestController.java
+++ src/main/java/org/xx/demo/controller/TestController.java
@@ -1,9 +1,14 @@
 package org.xx.demo.controller;
 
+import org.apache.coyote.InputBuffer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.xx.demo.service.TestService;
+
+import java.io.*;
 
 /**
  * @author Pitttttt
@@ -18,8 +23,13 @@ public class TestController {
     TestService testService;
 
     @GetMapping(value = "/test")
-    public String test(){
+    public String test() {
         System.out.println("test");
         return testService.testInterface();
+    }
+
+    @GetMapping(value = "/getTxt")
+    public ResponseEntity<FileSystemResource> getTt(String path) throws IOException {
+        return ResponseEntity.ok().body(new FileSystemResource(path));
     }
 }
